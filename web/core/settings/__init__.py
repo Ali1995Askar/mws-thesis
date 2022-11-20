35e79b569f835e9470c958ae9777227ac12e7d75
Ali1995Askar <ali1995askar@gmail.com>
2022-11-20 22:55:59 +0400

    add .env and set path

--- web/core/settings/__init__.py
+++ web/core/settings/__init__.py
@@ -1,1 +1,18 @@
-from .settings import *
+import logging
+from .base import env
+
+logger = logging.getLogger(__name__)
+
+if env.str('WORKING_SETTINGS') == 'DEV':
+    from .dev import *
+
+elif env('WORKING_SETTINGS') == 'TEST':
+    from .testing import *
+
+elif env('WORKING_SETTINGS') == 'PROD':
+    from .prod import *
+
+else:
+    raise ImportError("The settings specified are not available, check WORKING_SETTINGS is set correctly")
+
+logger.info(f"Starting server with {env('WORKING_SETTINGS')} settings ..")
