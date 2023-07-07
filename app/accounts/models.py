0ffcfca61ab93bb0dec413cb7bf040566adff022
Ali2023Askar <alipytechaskar@gmail.com>
2023-07-07 23:45:13 +0400

    company profile

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
-# Create your models here.
+phone_regex = RegexValidator(
+    regex=r'^\+?1?\d{9,15}$',
+    message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+
+
+class Profile(models.Model):
+    contact_email = models.EmailField(blank=False)
+    address = models.CharField(max_length=255, null=True)
+    logo = models.ImageField(upload_to='images/', null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    about = models.CharField(max_length=500, null=False, blank=False)
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, null=True)
