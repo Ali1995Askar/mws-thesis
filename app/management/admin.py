4130d95debd01eac3fa8c29e4b3b77721800227b
Ali2023Askar <alipytechaskar@gmail.com>
2023-07-19 00:52:23 +0400

    create manage,ent models

--- app/management/admin.py
+++ app/management/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from management import models
+
+
+@admin.register(models.BipartiteGraph)
+class BipartiteGraphAdmin(admin.ModelAdmin):
+    list_display = ['worker', 'task', 'user', 'created_on_datetime', 'updated_on_datetime']
+
+
+@admin.register(models.ExecutionHistory)
+class ExecutionHistoryAdmin(admin.ModelAdmin):
+    list_display = ['heuristic_matching', 'heuristic_matching', 'max_matching', 'heuristic_algorithm', 'execution_time']
--- app/management/models.py
+++ app/management/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class BipartiteGraph(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    worker = models.ForeignKey('workers.Worker', on_delete=models.CASCADE, null=False, blank=False)
+    task = models.ForeignKey('tasks.Task', on_delete=models.CASCADE, null=False, blank=False)
+    created_on_datetime = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_on_datetime = models.DateTimeField(auto_now=True, db_index=True)
+
+    class Meta:
+        unique_together = ('worker', 'task')
+
+
+class ExecutionHistory(models.Model):
+    class HeuristicAlgorithm(models.Choices):
+        STATIC_MIN_DEGREE = 'STATIC_MIN_DEGREE'
+        DYNAMIC_MIN_DEGREE = 'DYNAMIC_MIN_DEGREE'
+        LIMIT_MIN_DEGREE = 'LIMIT_MIN_DEGREE'
+
+        SIMPLE_GREEDY = 'SIMPLE_GREEDY'
+        MONTE_CARLO = 'MONTE_CARLO'
+        RANDOMIZED_ROUNDING = 'RANDOMIZED_ROUNDING'
+
+        MODIFIED_GREEDY = 'MODIFIED_GREEDY'
+
+    execution_time = models.FloatField(null=False, blank=False)
+    heuristic_matching = models.IntegerField(null=False, blank=False)
+    max_matching = models.IntegerField(null=False, blank=False)
+    heuristic_algorithm = models.CharField(max_length=50, db_index=True, choices=HeuristicAlgorithm.choices)
+    created_on_datetime = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_on_datetime = models.DateTimeField(auto_now=True, db_index=True)
