7ae33c0c67421cfe1c6161d16f0ae8ca93299b21
Ali2023Askar <alipytechaskar@gmail.com>
2023-08-07 12:20:06 +0400

    test graph builder

--- app/src/services/graph_builder.py
+++ app/src/services/graph_builder.py
@@ -1,17 +1,8 @@
-import time
-
-from django.db.models.functions import Concat
-
-from tasks.models import Task
 from typing import List, Tuple
-from workers.models import Worker
 from tasks.selectors import TaskSelectors
 from django.contrib.auth.models import User
 from src.services.edge_model.edge_model import Edge
 from src.graph.bipartite_graph import BipartiteGraph
-from django.db.models import QuerySet, Value, CharField, F
-
-from workers.selectors import WorkerSelectors
 
 
 class GraphBuilder:
@@ -21,46 +12,23 @@ class GraphBuilder:
         self.edges: List[Tuple[str, str]] = []
 
     def get_bipartite_graph(self):
-
-        free_workers, open_tasks = self.get_workers_and_tasks()
-        self.nodes = free_workers + open_tasks
-        self.edges = self.get_edges()
+        self.set_nodes_and_edges()
         bipartite_graph = self.build_graph()
-
         return bipartite_graph
 
-    def get_free_workers(self) -> List[str]:
-        free_workers = Worker.objects.filter(
-            user=self.user, status=Worker.Status.FREE
-        ).values_list('id', flat=True)
-        free_workers = list(free_workers)
-        return free_workers
+    def build_graph(self) -> BipartiteGraph:
+        inst = BipartiteGraph()
+        inst.build_manually(nodes=self.nodes, edges=self.edges)
+        return inst
 
-    def get_open_tasks(self) -> QuerySet[Task]:
-        open_tasks = Task.objects.filter(user=self.user, status=Task.Status.OPEN)
-        return open_tasks
-
-    def get_workers_and_tasks(self) -> Tuple[List[str], List[str]]:
-        free_workers = WorkerSelectors.get_free_workers_with_annotated_id(user=self.user)
-        open_tasks = TaskSelectors.get_free_tasks_with_annotated_id(user=self.user)
-        return free_workers, open_tasks
-
-    def get_edges(self) -> List[Tuple[str, str]]:
-        edges = []
-
+    def set_nodes_and_edges(self) -> None:
         tasks_workers_dict = TaskSelectors.get_tasks_with_connected_workers(user=self.user)
-
         for task, workers_qs in tasks_workers_dict.items():
             task_node_id = f'task-{task.id}'
             for worker_id in workers_qs:
                 worker_node_id = f'worker-{worker_id}'
                 edge_obj = Edge(task_id=task_node_id, worker_id=worker_node_id)
                 edge = edge_obj.as_tuple()
-                edges.append(edge)
-
-        return edges
-
-    def build_graph(self) -> BipartiteGraph:
-        inst = BipartiteGraph()
-        inst.build_manually(nodes=self.nodes, edges=self.edges)
-        return inst
+                self.nodes.append(task_node_id)
+                self.nodes.append(worker_node_id)
+                self.edges.append(edge)
--- app/src/services/tests/test_graph_builder.py
+++ app/src/services/tests/test_graph_builder.py
@@ -1,2 +1,50 @@
+import pytest
+from src.services.graph_builder import GraphBuilder
+from src.graph.bipartite_graph import BipartiteGraph
+
+
 class TestGraphBuilder:
-    pass
+    @pytest.fixture()
+    @pytest.mark.django_db(transaction=True)
+    def graph_builder(self, pytech_user):
+        inst = GraphBuilder(user=pytech_user)
+        return inst
+
+    @pytest.mark.django_db(transaction=True)
+    def test_build_and_get_bipartite_graph(self, graph_builder, mocker):
+        def fill_edges_and_nodes():
+            graph_builder.nodes = [1, 2, 3, 4, 5, 6, 6]
+            graph_builder.edges = [(1, 4), (2, 5), (3, 6)]
+
+        mocker.patch('src.services.graph_builder.GraphBuilder.set_nodes_and_edges', side_effect=fill_edges_and_nodes)
+
+        graph = graph_builder.get_bipartite_graph()
+        assert isinstance(graph, BipartiteGraph)
+        assert all(edge in [(1, 4), (2, 5), (3, 6), (4, 1), (5, 2), (6, 3)] for edge in graph.edges())
+        assert all(node in [1, 2, 3, 4, 5, 6] for node in graph.nodes())
+
+    @pytest.mark.django_db(transaction=True)
+    def test_set_nodes_and_edges(self,
+                                 graph_builder,
+                                 task_fix_tests,
+                                 task_pay_salaries,
+                                 task_send_emails,
+                                 worker_omar,
+                                 worker_ahmad,
+                                 worker_ali_askar,
+                                 mocker):
+        mocker.patch(
+            'tasks.selectors.TaskSelectors.get_tasks_with_connected_workers',
+            return_value={
+                task_fix_tests: [worker_ali_askar.id],
+                task_pay_salaries: [worker_omar.id],
+                task_send_emails: [worker_ahmad.id],
+            })
+        graph_builder.set_nodes_and_edges()
+
+        assert set(graph_builder.nodes) == {'task-1', 'worker-3', 'task-2', 'worker-1', 'task-3', 'worker-2'}
+        assert all(
+            edge in [('worker-3', 'task-1'), ('worker-1', 'task-2'), ('worker-2', 'task-3')]
+            for edge in graph_builder.edges
+        )
+        assert len(graph_builder.edges) == len([('worker-3', 'task-1'), ('worker-1', 'task-2'), ('worker-2', 'task-3')])
